2ce806029dfe289150210a816a8f8f01373326f0
yoviputranto <yoviputrantoo@gmail.com>
2021-02-20 08:36:39 +0700

    Tugas 4 rev 1

--- Tugas-5-Array/array.js
+++ Tugas-5-Array/array.js
@@ -35,21 +35,27 @@ function rangeWithStep(startNum, finishNum, step){
      }
      else {
          if (startNum>finishNum){
-             while(startNum>=finishNum){
+             for(var i=0; startNum>=finishNum; i++){
                  result.push(startNum)
                  startNum-=step
              }
          } else if(startNum<finishNum){
-            for(startNum; startNum<=finishNum; step){
+            for(var i; startNum<=finishNum; i++){
                 result.push(startNum)
+                startNum+=step
             }
          } else{
              result.push(startNum)
          }
      } console.log(result)
-}
+ }
 
 console.log(rangeWithStep(1, 10, 2)) // [1, 3, 5, 7, 9]
 console.log(rangeWithStep(11, 23, 3)) // [11, 14, 17, 20, 23]
 console.log(rangeWithStep(5, 2, 1)) // [5, 4, 3, 2]
 console.log(rangeWithStep(29, 2, 4)) // [29, 25, 21, 17, 13, 9, 5] 
+
+console.log("--------------")
+
+console.log("No 3")
+
